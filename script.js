5eaa9b91268be82008ab379b849c6aed0cb4b2f8
Stacy Betts <stacyo23@yahoo.com>
2020-11-21 09:32:41 -0600

    added git and linked-in buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#btn1").click(function() { 
+    document.location = "https://github.com/stacyo23";
+});
+
+$("#btn2").click(function() { 
+    window.open('https://www.linkedin.com/in/stacy-betts-fullstack/', '_blank');
+});
+
+// $("#btn3").click(function() { 
+//     window.open('https://www.linkedin.com/in/stacy-betts-fullstack/', '_blank');
+// });
