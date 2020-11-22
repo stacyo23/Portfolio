ef117b09966d0a28e8514736233635624609cf0c
Stacy Betts <stacyo23@yahoo.com>
2020-11-21 20:10:35 -0600

    added comments to eVeRyThInG

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-
+// button navigates to github repo
 $("#btn1").click(function() { 
     document.location = "https://github.com/stacyo23";
 });
 
+// button navigates to linkedin page
 $("#btn2").click(function() { 
     window.open('https://www.linkedin.com/in/stacy-betts-fullstack/', '_blank');
 });
 
-$("#sub").click(function() {
-var name= $("#name"); 
-var email= $("#email"); 
-var message= $("#message"); 
-name ="";
-email="";
-message=""; 
-});
-
-
+// button navigates to deployed gitfit site
 $("#GitFitD").click(function() { 
     document.location = "https://lkglass-smu.github.io/GitFit/";
 });
 
+// button navigates to gitfit repo
 $("#GitFitR").click(function() { 
     document.location = "https://github.com/LKglass-SMU/GitFit";
 });
 
+// button navigates to deployed password generator site
 $("#passGenD").click(function() { 
     document.location = "https://stacyo23.github.io/PasswordGenerator/";
 });
 
+// button navigates to password generator repo
 $("#passGenR").click(function() { 
     document.location = "https://github.com/stacyo23/PasswordGenerator";
 });
 
+// button navigates to deployed workday scheduler site
 $("#scheduleD").click(function() { 
     document.location = "https://stacyo23.github.io/WorkDayScheduler/";
 });
 
+// button navigates to workday scheduler repo
 $("#scheduleR").click(function() { 
     document.location = "https://github.com/stacyo23/WorkDayScheduler";
 });
