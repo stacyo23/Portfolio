fa29696a6f0571abbb0d7caa95313b1483504992
Stacy Betts <stacyo23@yahoo.com>
2020-11-21 19:14:54 -0600

    deleted some extra photos, added pics and buttons to portfolio page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $("#btn1").click(function() { 
     document.location = "https://github.com/stacyo23";
 });
@@ -6,6 +7,37 @@ $("#btn2").click(function() {
     window.open('https://www.linkedin.com/in/stacy-betts-fullstack/', '_blank');
 });
 
-// $("#btn3").click(function() { 
-//     window.open('https://www.linkedin.com/in/stacy-betts-fullstack/', '_blank');
-// });
+$("#sub").click(function() {
+var name= $("#name"); 
+var email= $("#email"); 
+var message= $("#message"); 
+name ="";
+email="";
+message=""; 
+});
+
+
+$("#GitFitD").click(function() { 
+    document.location = "https://lkglass-smu.github.io/GitFit/";
+});
+
+$("#GitFitR").click(function() { 
+    document.location = "https://github.com/LKglass-SMU/GitFit";
+});
+
+$("#passGenD").click(function() { 
+    document.location = "https://stacyo23.github.io/PasswordGenerator/";
+});
+
+$("#passGenR").click(function() { 
+    document.location = "https://github.com/stacyo23/PasswordGenerator";
+});
+
+$("#scheduleD").click(function() { 
+    document.location = "https://stacyo23.github.io/WorkDayScheduler/";
+});
+
+$("#scheduleR").click(function() { 
+    document.location = "https://github.com/stacyo23/WorkDayScheduler";
+});
+
